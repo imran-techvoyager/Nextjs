1f1df394697bb38307d55c80072ded4bc481ed4e
Imran Ahmed <imran.techvoyager@gmail.com>
2025-07-10 20:09:44 +0530

    feat(signup): implement signup form with input fields and submit handler

--- routing/src/app/(auth)/signup/page.tsx
+++ routing/src/app/(auth)/signup/page.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
 
 const Signup = () => {
+
+     const handleSubmit = () => {
+        
+     }
+
   return (
-    <div>Signup</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input type="text" placeholder='Enter your name'/>
+            <input type="email" placeholder='Enter your email'/>
+            <input type="password" placeholder='Enter your password'/>
+            <button type='submit'>submit</button>
+        </form>
+    </div>
   )
 }
 
