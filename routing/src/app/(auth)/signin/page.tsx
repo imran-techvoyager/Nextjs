afcaba0c83ec027b1b8a91c308b16c2316442d74
Imran Ahmed <imran.techvoyager@gmail.com>
2025-07-10 20:10:51 +0530

    feat(signin): implement signin form with email and password inputs

--- routing/src/app/(auth)/signin/page.tsx
+++ routing/src/app/(auth)/signin/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
 const Signin = () => {
+
+     const handleSubmit = () => {
+        
+     }
+
   return (
-    <div>Signin</div>
+    <div>
+        <form onSubmit={handleSubmit}>
+            <input type="email" placeholder='Enter your email'/>
+            <input type="password" placeholder='Enter your password'/>
+            <button type='submit'>submit</button>
+        </form>
+    </div>
   )
 }
 
